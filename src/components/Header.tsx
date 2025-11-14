5360fbeddbc358c10aa7e563ef5003eced3412c7
Pratyush Raj <pratyushraj@Pratyushs-MacBook-Air.local>
2025-11-15 04:02:46 +0530

    Revert "Use header-logo.jpg to avoid URL encoding issues"
    This reverts commit e668dc78fee8d8ad844afa12d5e77e3aa277dfab.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -42,7 +42,7 @@ const Header = () => {
       <div className="flex h-16 items-center justify-between py-4 px-3 md:px-4">
         <Link to={dashboardPath} className="flex items-center space-x-3">
           <img
-            src="/header-logo.jpg"
+            src="/Ca%2Blawyer.jpg"
             alt="NoticeBazaar"
             className="h-10 w-auto rounded-md object-cover"
           />
