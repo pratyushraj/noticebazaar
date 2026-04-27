e69c5b610a8a6af9b029d8d685979c403645435b
pratyushraj <pratyushraj@outlook.com>
2026-04-27 20:47:29 +0530

    fix: bump app shell version to force cache cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const queryClient = new QueryClient({
   },
 });
 
-const APP_SHELL_VERSION = '2026-04-25-2';
+const APP_SHELL_VERSION = '2026-04-27-3';
 
 const RouterInstrumentation = () => {
   usePerformanceMonitoring();
