69a89272d429ce18dbd2600ab8c8f333233678aa
pratyushraj <pratyushraj@outlook.com>
2026-05-16 00:54:45 +0530

    fix: wrap app with HelmetProvider to resolve runtime crash

--- src/main.tsx
+++ src/main.tsx
@@ -54,6 +54,7 @@ silenceConsoleInProduction();
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./globals.css";
 import { initWebVitals } from "./lib/utils/webVitals";
@@ -71,7 +72,9 @@ const root = createRoot(document.getElementById("root")!);
 root.render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </QueryClientProvider>
 );
